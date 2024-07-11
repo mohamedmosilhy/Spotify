6ff252f7a067ff116505fb74ca8235421ba40a11
mohamed mosilhy <93820559+mohamedmosilhy@users.noreply.github.com>
2024-07-11 15:49:50 +0300

    add splash and getstarted pages

---
+++ lib/core/config/assets/app_vector.dart
@@ -0,0 +1,5 @@
+class AppVector {
+  static const String basePath = 'assets/vectors/';
+  static const String format = '.svg';
+  static const String logo = '${basePath}spotify_logo$format';
+}
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:spotify/core/config/theme/app_theme.dart';
+import 'package:spotify/presentation/get_started/pages/get_started.dart';
+import 'package:spotify/presentation/splash/pages/splash.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    redirect();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppTheme.lightTheme,
+      home: const SplashPage(),
+    );
+  }
+
+  Future<void> redirect() async {
+    await Future.delayed(const Duration(seconds: 2));
+    Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+            builder: (BuildContext context) => const GetStartedPage()));
+  }
+}
